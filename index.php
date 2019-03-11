6e9340325cb68bd4a2ec2db7614d2f3b26c772c4
juangallego9810 <48457148+juangallego9810@users.noreply.github.com>
2019-03-11 16:58:23 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Sistema de examen en línea inteligente</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style2.css">
+
+  
+</head>
+
+<body>
+<div>
+<center><b><h4>sistema de examen en línea inteligente tusolutionweb</h4></b></center>
+</div>
+  <div class='one'>
+  <a href="admin/"><button class='indigo ripple' ripple-color='#5C6BC0'>Admin Login tusolutionweb</button></a>
+</div>
+<div class='two'>
+  <a href="student/"><button class='white ripple' ripple-color='white'>Acceso de Estudiante</button></a>
+</div>
+
+
+</body>
+
+</html>
